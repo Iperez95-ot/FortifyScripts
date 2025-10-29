3eef13fffcd11e9c1bad407ce349bda86ddafee4
Ignacio Perez Civeira <iperez@opentext.com>
2025-10-29 02:45:03 -0300

    Delete tomcat automation scripts/linux directory

--- tomcat automation scripts/linux/setenv.sh
+++
@@ -1,7 +0,0 @@
-export JAVA_HOME="yourpathtojavaonlinux"
-
-export CATALINA_HOME="yourpathtotomcatonlinux"
-
-export CATALINA_OPTS="$CATALINA_OPTS -Xms2048m"
-
-export CATALINA_OPTS="$CATALINA_OPTS -Xmx4096m"
